3d2c7f79fca782e216309e68feb848ba99d71df1
Johan Andersson <nilsjohanandersson@gmail.com>
2017-08-30 22:20:36 +0200

    Build libSOIL.dll

--- src/SOIL.h
+++ src/SOIL.h
@@ -40,6 +40,12 @@
 #ifndef HEADER_SIMPLE_OPENGL_IMAGE_LIBRARY
 #define HEADER_SIMPLE_OPENGL_IMAGE_LIBRARY
 
+#ifdef SOIL_DLL_EXPORTING
+#define SOIL_DLL_EXPORT __declspec(dllexport)
+#else
+#define SOIL_DLL_EXPORT __declspec(dllimport)
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -149,6 +155,7 @@ enum
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_texture
 	(
@@ -171,6 +178,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_cubemap
 	(
@@ -194,6 +202,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_single_cubemap
 	(
@@ -212,6 +221,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_HDR_texture
 	(
@@ -231,6 +241,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_texture_from_memory
 	(
@@ -260,6 +271,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_cubemap_from_memory
 	(
@@ -290,6 +302,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_load_OGL_single_cubemap_from_memory
 	(
@@ -312,6 +325,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_create_OGL_texture
 	(
@@ -332,6 +346,7 @@ unsigned int
 	\param flags can be any of SOIL_FLAG_POWER_OF_TWO | SOIL_FLAG_MIPMAPS | SOIL_FLAG_TEXTURE_REPEATS | SOIL_FLAG_MULTIPLY_ALPHA | SOIL_FLAG_INVERT_Y | SOIL_FLAG_COMPRESS_TO_DXT | SOIL_FLAG_DDS_LOAD_DIRECT
 	\return 0-failed, otherwise returns the OpenGL texture handle
 **/
+SOIL_DLL_EXPORT
 unsigned int
 	SOIL_create_OGL_single_cubemap
 	(
@@ -346,6 +361,7 @@ unsigned int
 	Captures the OpenGL window (RGB) and saves it to disk
 	\return 0 if it failed, otherwise returns 1
 **/
+SOIL_DLL_EXPORT
 int
 	SOIL_save_screenshot
 	(
@@ -364,6 +380,7 @@ int
 	count).
 	\return 0 if failed, otherwise returns 1
 **/
+SOIL_DLL_EXPORT
 unsigned char*
 	SOIL_load_image
 	(
@@ -381,6 +398,7 @@ unsigned char*
 	count).
 	\return 0 if failed, otherwise returns 1
 **/
+SOIL_DLL_EXPORT
 unsigned char*
 	SOIL_load_image_from_memory
 	(
@@ -394,6 +412,7 @@ unsigned char*
 	Saves an image from an array of unsigned chars (RGBA) to disk
 	\return 0 if failed, otherwise returns 1
 **/
+SOIL_DLL_EXPORT
 int
 	SOIL_save_image
 	(
@@ -408,6 +427,7 @@ int
 	present mostly so C++ programmers don't forget to use "free()" and call
 	"delete []" instead [8^)
 **/
+SOIL_DLL_EXPORT
 void
 	SOIL_free_image_data
 	(
@@ -419,6 +439,7 @@ void
 	that happened inside SOIL.  It can be used to determine why an image
 	failed to load.
 **/
+SOIL_DLL_EXPORT
 const char*
 	SOIL_last_result
 	(
